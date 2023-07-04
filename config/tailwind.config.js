fa7b940527495e0ca19ed9602c75ab3888cfefed
immprada <im.mprada@gmail.com>
2023-07-04 07:18:38 -0500

    INITIAL SETUP

---
+++ config/tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      'black-dadrk': '#0B090B',
+      'black': '#120F14',
+      'black-soft': '#252329',
+      'grey': '#3C393F',
+      'grey-soft': '#828282',
+      'blue': '#3081ED',
+      'white': '#ffffff'
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
