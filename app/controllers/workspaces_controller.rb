daa3425483cd59e9f1fa9c54eff8fc19183bc473
immprada <im.mprada@gmail.com>
2023-07-05 18:46:34 -0500

    create a workspace

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -7,7 +7,11 @@ class WorkspacesController < ApplicationController
 
   def create
     @workspace_creator = Workspaces::Creator.new(workspace_params, current_user)
-    return invalid_workspace unless workspace_creator.workspace_valid?
+    # return invalid_workspace unless workspace_creator.workspace_valid?
+
+    valid_workspace
+  rescue StandardError => e
+    return_by_error(e.message)
   end
 
   private
@@ -20,6 +24,17 @@ class WorkspacesController < ApplicationController
 
   def invalid_workspace
     flash[:alert] = workspace_creator.errors.full_messages.join(', ')
+    redirect_to profile_path(current_user)
+  end
+
+  def valid_workspace
+    workspace_creator.commit
+    flash[:notice] = "#{workspace_creator.name} created successfully"
+    redirect_to profile_path(current_user)
+  end
+
+  def return_by_error(message)
+    flash[:alert] = message
     redirect_to profile_path(current_user)
   end
 end
