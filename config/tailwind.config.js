411c5fa119deef8f7dba10d54ceb8172ce24e0d6
immprada <im.mprada@gmail.com>
2023-07-04 22:14:18 -0500

    roll back tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,19 +12,6 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
-    colors: {
-      'black-dadrk': '#0B090B',
-      'black': '#120F14',
-      'black-soft': '#252329',
-      'grey': '#3C393F',
-      'grey-soft': '#828282',
-      'blue': '#3081ED',
-      'white': '#ffffff',
-      'red-soft': '#FDA4AF',
-      'red': '#BE113C',
-      'green-soft': '#BBF7D0',
-      'green': '#18A34A'
     }
   },
   plugins: [
