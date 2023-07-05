660acbb147241235fa2de1679a2641c547a82410
immprada <im.mprada@gmail.com>
2023-07-05 18:26:24 -0500

    add WorkspacesController#create invalid workspace case

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -4,4 +4,22 @@ class WorkspacesController < ApplicationController
   def new
     @workspace = Workspace.new
   end
+
+  def create
+    @workspace_creator = Workspaces::Creator.new(workspace_params, current_user)
+    return invalid_workspace unless workspace_creator.workspace_valid?
+  end
+
+  private
+
+  attr_reader :workspace_creator
+
+  def workspace_params
+    params.require(:workspace).permit(:name)
+  end
+
+  def invalid_workspace
+    flash[:alert] = workspace_creator.errors.full_messages.join(', ')
+    redirect_to profile_path(current_user)
+  end
 end
