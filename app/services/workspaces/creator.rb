3cbe6aae7f22ad76093110e0611996147396189e
immprada <im.mprada@gmail.com>
2023-07-05 18:46:21 -0500

    complete Workspaces::Creator process

--- app/services/workspaces/creator.rb
+++ app/services/workspaces/creator.rb
@@ -1,12 +1,11 @@
 module Workspaces
   class Creator
     def initialize(params, user)
-      @params = params
+      @workspace = Workspace.new(params)
       @user = user
     end
 
     def workspace_valid?
-      @workspace = Workspace.new(@params)
       workspace.valid?
     end
 
@@ -16,8 +15,28 @@ module Workspaces
       workspace.errors
     end
 
+    def name
+      workspace.name
+    end
+
+    def commit
+      raise return_error unless workspace.valid?
+
+      workspace.save!
+      owner_role = Role.find_by(name: 'owner')
+      workspace_user = WorkspaceUser.new(user:, workspace:, role: owner_role)
+
+      return workspace if workspace_user.save
+
+      raise return_error
+    end
+
     private
 
-    attr_reader :params, :user, :workspace
+    attr_reader :user, :workspace
+
+    def return_error
+      StandardError.new 'Something went wrong, please try again'
+    end
   end
 end
