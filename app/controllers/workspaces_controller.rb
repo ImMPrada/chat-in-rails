3e022a896acf4e4ad84658a3c1e512b6fca00540
immprada <im.mprada@gmail.com>
2023-07-05 22:04:01 -0500

    update list of user's workspaces with stream

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -28,13 +28,23 @@ class WorkspacesController < ApplicationController
   end
 
   def valid_workspace
-    workspace_creator.commit
-    flash[:notice] = "#{workspace_creator.name} created successfully"
-    redirect_to profile_path(current_user)
+    new_workspace = workspace_creator.commit
+    flash.now[:notice] = "#{workspace_creator.name} created successfully"
+
+    update_user_workspaces(new_workspace)
   end
 
   def return_by_error(message)
     flash[:alert] = message
     redirect_to profile_path(current_user)
+  end
+
+  def update_user_workspaces(workspace)
+    render turbo_stream: [
+      turbo_stream.append(:my_workspaces_list,
+                          partial: 'partials/workspaces/list_card',
+                          locals: { workspace: }),
+      turbo_stream.update(:new_workspace_form, '')
+    ]
   end
 end
