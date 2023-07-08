8be916278665550e3024f263d4f7b455130b2eca
immprada <im.mprada@gmail.com>
2023-07-08 08:25:24 -0500

    chore(channel model): add description field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_205443) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_132428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_205443) do
     t.bigint "workspace_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["workspace_id"], name: "index_channels_on_workspace_id"
   end
 
