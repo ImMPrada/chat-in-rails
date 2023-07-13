3de85a5ae3663cd5357ca529b8ede9cfc72f4a8d
immprada <im.mprada@gmail.com>
2023-07-13 16:56:37 -0500

    update some code style

--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -2,9 +2,7 @@ module DatesHelper
   def friendly_date(date)
     today = Time.zone.today
     date = date.to_date
-
     return "today at #{date.strftime('%l:%M %p')}" if date == today
-
     return "yesterday at #{date.strftime('%l:%M %p')}" if date == today - 1
 
     "#{date.strftime('%B %e, %Y')} at #{date.strftime('%l:%M %p')}"
