94268414e600fbc779d54abeb0183679ece6617d
immprada <im.mprada@gmail.com>
2023-07-18 04:44:37 -0500

    update by discussion

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
   def create
-    @message_box_turbo_tag = message_box_turbo_tag
-    @url = url
+    @workspace = Workspace.find(params[:workspace_id])
 
     message_creator = Messages::Creator.new(
       permitted_params,
@@ -10,21 +9,20 @@ class MessagesController < ApplicationController
     )
     message = message_creator.commit
 
-    message ? respond_with_succes(message) : respond_with_failure(message_creator)
+    message ? respond_with_success(message) : respond_with_failure(message_creator)
 
     respond_to { |format| format.turbo_stream }
   end
 
   private
 
-  def respond_with_succes(message)
-    @message_succed = true
-    @messages_turbo_tag = messages_turbo_tag
+  def respond_with_success(message)
+    @message_creation_result = :success
     @message = message
   end
 
   def respond_with_failure(message_creator)
-    @message_succed = false
+    @message_creation_result = :failure
     @message = message_creator.errors_messages.join(', ')
   end
 
@@ -32,39 +30,13 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content)
   end
 
-  def workspace
-    @workspace ||= Workspace.find(params[:workspace_id])
-  end
-
   def destination
-    @destination ||= find_record
+    @destination ||= find_destination
   end
 
-  def find_record
+  def find_destination
     return WorkspaceChannel.find(params[:channel_id]) if params[:channel_id]
 
     User.find(params[:user_id])
-  end
-
-  def url
-    return workspace_channel_path(workspace, destination) if destination.instance_of? WorkspaceChannel
-
-    workspace_user_messages_path(workspace, destination)
-  end
-
-  def message_box_turbo_tag
-    return "workspace_channel_#{destination.id}_message_box" if workspace_channel?
-
-    "user_#{destination.id}_message_box"
-  end
-
-  def messages_turbo_tag
-    return "workspace_channel_#{destination.id}_messages" if workspace_channel?
-
-    "user_#{destination.id}_messages"
-  end
-
-  def workspace_channel?
-    destination.instance_of? WorkspaceChannel
   end
 end
--- app/models/workspace_channel.rb
+++ app/models/workspace_channel.rb
@@ -1,4 +1,6 @@
 class WorkspaceChannel < ApplicationRecord
+  include Destinationable
+
   before_create :add_avatar_url
 
   validates :name,
@@ -14,7 +16,19 @@ class WorkspaceChannel < ApplicationRecord
   has_many :users, through: :workspace_channel_users
   has_many :messages, as: :classifiable, dependent: :destroy
 
+  def message_box_turbo_tag
+    "workspace_channel_#{id}_message_box"
+  end
+
+  def messages_turbo_tag
+    "workspace_channel_#{id}_messages"
+  end
+
   private
+
+  def destination_path
+    :workspace_channel_messages_path
+  end
 
   def add_avatar_url
     self.avatar_url = "https://ui-avatars.com/api/?background=3c393f&color=fff&rounded=false&bold=true&name=#{CGI.escape(name)}"
