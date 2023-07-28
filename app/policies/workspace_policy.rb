316fbc31d19c51a4b43abfcbe398a1d140971cd4
immprada <im.mprada@gmail.com>
2023-07-28 07:48:07 -0500

    refactor(remove cancancan): - remove inlusion into Model - implement instead, pundit policy

--- app/policies/workspace_policy.rb
+++ app/policies/workspace_policy.rb
@@ -11,6 +11,14 @@ class WorkspacePolicy < ApplicationPolicy
     user.admin?(workspace)
   end
 
+  def add_member?
+    user.admin?(workspace)
+  end
+
+  def add_channel?
+    user.admin?(workspace)
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
